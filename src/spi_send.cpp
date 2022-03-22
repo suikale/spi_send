07ce1ff4559d8f8adf3c9c66ea57ead749dc45ca
suikale <suikale@kaappi.pw>
2022-03-22 17:03:27 +0200

    skeleton for a new controller

--- src/spi_send.cpp
+++ src/spi_send.cpp
@@ -1,15 +1,15 @@
 /// @author suikale
-/// @version 01112021
+/// @version 22032022
 /// depends: wiringPi
+
 #include <wiringPi.h>
 #include <wiringPiSPI.h>
 #include <iostream>
 #include <string>
 
 #define SPI_CHANNEL 	0
-#define SPI_CLOCK_SPEED 2000000
+#define SPI_CLOCK_SPEED 250000
 #define RESET_PIN 		22
-#define MAX_ARGS 		3
 
 void spi_rw(unsigned char *data, int len)
 {
@@ -21,11 +21,8 @@ int main(int argc, char **argv)
     if (argc < 2)
 	{
 		std::cout << "usage: " << std::endl;
-		std::cout << "  set single socket state: " << argv[0] << " 0/1 [id] [group]" << std::endl;
-		std::cout << "  set group state:         " << argv[0] << " 2/3 [group]" << std::endl;
-		std::cout << "  reset socket:            " << argv[0] << " r [id] [group]" << std::endl;
-		std::cout << "  reset avr:               " << argv[0] << " hr" << std::endl;
-		std::cout << "    where [id/group] = 0 ... 3" << std::endl;
+		std::cout << "  set state: " << argv[0] << " [state] [id] [group] [remote]" << std::endl;
+		std::cout << "TODO:  reset avr: " << argv[0] << " hr" << std::endl;
 		return -1;
 	}
 	 
@@ -42,22 +39,17 @@ int main(int argc, char **argv)
 		std::cout << "avr reset" << std::endl;
 		return 0;
 	}
-
 	
-	char c[MAX_ARGS];
-	for (int i = 0; i < MAX_ARGS; i++)
-		c[i] = 0;
+	// hardcoded values for remote 1, device 0, group 0, state 1
+	unsigned char d[5];
+	d[0] = 0x00;
+	d[1] = 0x86;
+	d[2] = 0x80;
+	d[3] = 0x2a;
+	d[4] = 0x81;
+	spi_rw(d, 5);	
 	
-	for (int i = 1; i < argc && i <= MAX_ARGS; i++)
-		c[i - 1] = *argv[i];
-	
-	for (int i = 0; i < argc; i++)
-	{
-		unsigned char j = (unsigned char) *(c + i);
-		spi_rw(&j, 1);
-	}
-	
-	std::cout << "sent " << argc - 1 << " bytes, received: " << c << std::endl;
+	printf("sent %d bytes\n", sizeof(d));
 	
 	return 0;
 }
